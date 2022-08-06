a0caf6c14e270d6ebcf23d8c24ce41ccf6b12623
jungsunghun <jsh951227@gmail.com>
2022-08-06 16:59:33 +0900

    add : SessionArgumentResolver 추가

--- src/main/java/com/blog/blogservice/processor/config/WebConfig.java
+++ src/main/java/com/blog/blogservice/processor/config/WebConfig.java
@@ -1,12 +1,16 @@
 package com.blog.blogservice.processor.config;
 
+import com.blog.blogservice.processor.argumentResolver.MemberArgumentResolver;
 import com.blog.blogservice.processor.interceptor.session.ChainNo1;
 import com.blog.blogservice.processor.interceptor.session.ChainNo2;
 import com.blog.blogservice.processor.interceptor.session.SessionCheckChain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
@@ -20,5 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1);
         registry.addInterceptor(chainNo2)
                 .order(2);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberArgumentResolver());
     }
 }
