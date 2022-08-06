ffff35d8a5849d1b906a75a53a67eeb2e1f4183a
jungsunghun <jsh951227@gmail.com>
2022-08-07 01:32:21 +0900

    add : @Valid 기능을 이용한 검증 추가

--- src/main/java/com/blog/blogservice/controller/dto/request/LoginForm.java
+++ src/main/java/com/blog/blogservice/controller/dto/request/LoginForm.java
@@ -3,17 +3,22 @@ package com.blog.blogservice.controller.dto.request;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.util.StringUtils;
 
 import javax.validation.constraints.NotEmpty;
+
+import static com.blog.blogservice.processor.config.ViewNameConfig.LOGIN_FORM;
 
 @Data
 @NoArgsConstructor
 public class LoginForm {
 
+    @NotEmpty(message = "아이디를 입력해주세요.")
     private String loginId;
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
     private String password;
+
     private boolean rememberId;
+    private final String exOccursView = LOGIN_FORM;
 
     @Builder
     public LoginForm(String loginId, String password) {
