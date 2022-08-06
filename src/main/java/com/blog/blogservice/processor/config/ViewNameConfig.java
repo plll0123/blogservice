6352becedec8d37d02d647cb74153548d36e2e1b
jungsunghun <jsh951227@gmail.com>
2022-08-07 01:29:19 +0900

    add : View의 경로를 static 상수로 정의한 클래스

---
+++ src/main/java/com/blog/blogservice/processor/config/ViewNameConfig.java
@@ -0,0 +1,7 @@
+package com.blog.blogservice.processor.config;
+
+public abstract class ViewNameConfig {
+
+    public static final String REDIRECT_HOME = "redirect:/";
+    public static final String LOGIN_FORM = "system/login";
+}
