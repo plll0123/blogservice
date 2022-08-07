bd4213252b337ad6f4749f95453af1b320a8250a
jungsunghun <jsh951227@gmail.com>
2022-08-07 20:48:06 +0900

    add : Blog 엔티티 생성 코드

--- src/main/java/com/blog/blogservice/service/BlogService.java
+++ src/main/java/com/blog/blogservice/service/BlogService.java
@@ -1,23 +1,34 @@
 package com.blog.blogservice.service;
 
-import com.blog.blogservice.repository.BlogRepository;
-import com.blog.blogservice.repository.MemberRepository;
+import com.blog.blogservice.controller.dto.request.BlogCreate;
+import com.blog.blogservice.domain.Blog;
+import com.blog.blogservice.domain.Member;
+import com.blog.blogservice.repository.entityrepo.BlogRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
+import static com.blog.blogservice.domain.Blog.createBlog;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BlogService {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final BlogRepository blogRepository;
 
-//    public Long create(BlogCreate blogCreate){
-//
-//    }
+    @Transactional
+    public Long create(Long memberId, BlogCreate blogCreate){
+
+        Member findMember = memberService.findById(memberId);
+
+        return blogRepository.save(createNewBlog(findMember, blogCreate))
+                .getId();
+    }
+
+    public Blog createNewBlog(Member member, BlogCreate blogCreate) {
+        return createBlog(member, blogCreate.getTitle(), blogCreate.getTag());
+    }
 
 }
