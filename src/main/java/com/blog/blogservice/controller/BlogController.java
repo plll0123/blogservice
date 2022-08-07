2457ee9bdbb3da2629caaf3308df18bcc4dbfc88
jungsunghun <jsh951227@gmail.com>
2022-08-07 20:47:04 +0900

    -

--- src/main/java/com/blog/blogservice/controller/BlogController.java
+++ src/main/java/com/blog/blogservice/controller/BlogController.java
@@ -1,20 +1,29 @@
 package com.blog.blogservice.controller;
 
+import com.blog.blogservice.controller.dto.request.BlogCreate;
+import com.blog.blogservice.domain.Member;
+import com.blog.blogservice.processor.annotation.Login;
 import com.blog.blogservice.processor.annotation.RequiredLogin;
+import com.blog.blogservice.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/blog")
 public class BlogController {
 
+    private final BlogService blogService;
+
     @RequiredLogin
     @PostMapping
-    public String register(){
+    public String register(@Valid BlogCreate blogCreate, @Login Member member){
 
+        blogService.create(member.getId(), blogCreate);
         return null;
     }
 }
