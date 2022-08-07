6ee0a75f20d8de25377f0e5d93d1f672cb4578e8
jungsunghun <jsh951227@gmail.com>
2022-08-07 20:43:54 +0900

    add : 블로그 생성 요청에 실패시 이동할 뷰 선택

--- src/main/java/com/blog/blogservice/processor/config/ViewNameConfig.java
+++ src/main/java/com/blog/blogservice/processor/config/ViewNameConfig.java
@@ -4,4 +4,5 @@ public abstract class ViewNameConfig {
 
     public static final String REDIRECT_HOME = "redirect:/";
     public static final String LOGIN_FORM = "system/login";
+    public static final String INSERT_BLOG = "blog/insertBlog";
 }
